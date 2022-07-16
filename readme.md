8d7edf2a36a373eb35667f2bbc206a9e54595a53
SleepyIntoxicator <Alsakharov@mail.ru>
2022-07-16 18:57:46 +0300

    Added task 704. Binary Search; updated readme.md

--- readme.md
+++ readme.md
@@ -1,4 +1,7 @@
 # LeetCode Solved Problems
 
-    9. Palindrome Number
-    14. Longest Common Prefix
+| Status | Number | Name                  | Link |
+|--------|--------|-----------------------|------|
+| [x]    | 9      | Palindrome Number     |      |
+| [x]    | 14     | Longest Common Prefix |      |
+| [x]    | 704    | Binary Search         |      |
---
+++ solutions/704. Binary Search_test.go
@@ -0,0 +1,50 @@
+package solutions
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestSearch(t *testing.T) {
+	type TestCase struct {
+		Name     string
+		Nums     []int
+		Target   int
+		Expected int
+	}
+
+	tcs := []TestCase{
+		{
+			Name:     "search in even number of numbers",
+			Nums:     []int{1, 2, 3, 4, 5, 6, 7, 8},
+			Target:   5,
+			Expected: 4,
+		},
+		{
+			Name:     "search in odd number of numbers",
+			Nums:     []int{1, 2, 3, 4, 5, 6, 7},
+			Target:   4,
+			Expected: 3,
+		},
+		{
+			Name:     "search for a missing number",
+			Nums:     []int{1, 2, 3, 4, 6, 7, 8},
+			Target:   5,
+			Expected: -1,
+		},
+		{
+			Name:     "search in a null array of numbers",
+			Nums:     []int{},
+			Target:   0,
+			Expected: -1,
+		},
+	}
+
+	for _, tc := range tcs {
+		t.Run("704. BinarySearch\\"+tc.Name, func(t *testing.T) {
+			out := Search(tc.Nums, tc.Target)
+
+			assert.Equal(t, tc.Expected, out)
+		})
+	}
+}
