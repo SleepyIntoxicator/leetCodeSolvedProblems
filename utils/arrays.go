1e13aa9035ee29a75de1e0765fe98491c9153c5a
SleepyIntoxicator <Alsakharov@mail.ru>
2022-07-19 22:47:34 +0300

    Added utils for testing; added a method for checking lists (arrays, slices, strings) for matching elements in addition to the 'assert' package; added appropriate methods for checking for compliance with the list type (array, slice, string) and checking for the presence of elements; added tests.

---
+++ utils/arrays.go
@@ -0,0 +1,114 @@
+package utils
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"reflect"
+	"testing"
+)
+
+// AssertEqualLists asserts that the specified expected(array, slice, string...) is equal to specified
+// actual(array, slice, string).
+func AssertEqualLists(t *testing.T, expected, actual interface{}) bool {
+	if !isList(t, expected) || !isList(t, actual) {
+		//if reflect.ValueOf(expected).Kind() == reflect.String &&
+		//	reflect.ValueOf(actual).Kind() == reflect.String {
+		//	expected = expected.([]byte)
+		//	actual = actual.([]byte)
+		//} else {
+		//	return false
+		//}
+		return false
+	}
+
+	aValue := reflect.ValueOf(expected)
+	bValue := reflect.ValueOf(actual)
+
+	if isListOrMapEmpty(expected) && isListOrMapEmpty(actual) {
+		return true
+	}
+
+	expectedLen := aValue.Len()
+	actualLen := bValue.Len()
+
+	if expectedLen != actualLen {
+		var msg bytes.Buffer
+
+		msg.WriteString("elements differ")
+		msg.WriteString(fmt.Sprintf("\nexpected:\t%v", aValue))
+		msg.WriteString(fmt.Sprintf("\nactual:\t\t%v", bValue))
+		assert.Fail(t, msg.String(), nil)
+		return false
+	}
+
+	var isIntSlice bool
+	if expectedLen > 0 {
+		//TODO: compare with aValue.Type().Elem().String() == "int"
+		isIntSlice = reflect.ValueOf(aValue.Index(0).Interface()).Kind() == reflect.Int
+	}
+
+	var diff []byte
+	if isIntSlice {
+		diff = make([]byte, 0, expectedLen)
+	}
+
+	areEqual := true
+	for i := 0; i < expectedLen; i++ {
+		expected := aValue.Index(i).Interface()
+		actual := bValue.Index(i).Interface()
+
+		if !assert.ObjectsAreEqual(expected, actual) {
+			areEqual = false
+			if isIntSlice {
+				diff = append(diff, '-')
+			}
+		} else if isIntSlice {
+			diff = append(diff, ' ')
+		}
+	}
+
+	if !areEqual {
+		var msg bytes.Buffer
+
+		msg.WriteString("elements differ")
+		msg.WriteString(fmt.Sprintf("\nexpected:\t%v", aValue))
+		if isIntSlice {
+			msg.WriteString(fmt.Sprintf("\ndiff:\t\t%c", diff))
+		}
+		msg.WriteString(fmt.Sprintf("\nactual:\t\t%v", bValue))
+		assert.Fail(t, msg.String())
+		return false
+	}
+
+	return true
+}
+
+// isList checks that the provided value is array, slice or string.
+func isList(t *testing.T, list interface{}) (ok bool) {
+	kind := reflect.TypeOf(list).Kind()
+	switch kind {
+	case reflect.Array, reflect.Slice, reflect.String:
+		return true
+	case reflect.Map:
+		assert.Fail(t, fmt.Sprintf("%v has unsupported type '%s', expecting array, slice or string", list, kind))
+		return false
+	default:
+		assert.Fail(t, fmt.Sprintf("%q has unsupported type '%s', expecting array, slice or string", list, kind))
+	}
+
+	return false
+}
+
+// isListOrMapEmpty gets whether the specified list or map is empty or not.
+// Returns false for all other types of object.
+func isListOrMapEmpty(object interface{}) bool {
+	objValue := reflect.ValueOf(object)
+
+	switch objValue.Kind() {
+	case reflect.Slice, reflect.Array, reflect.String, reflect.Map:
+		return objValue.Len() == 0
+	default:
+		return false
+	}
+}
---
+++ utils/arrays_test.go
@@ -0,0 +1,222 @@
+package utils
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestAssertEqualSlices(t *testing.T) {
+	type TestCase struct {
+		Name           string
+		Expected       interface{}
+		Actual         interface{}
+		ExpectedResult bool
+	}
+
+	tcs := []TestCase{
+		{
+			Name:           "Equal int slices",
+			Expected:       []int{1, 2, 3},
+			Actual:         []int{1, 2, 3},
+			ExpectedResult: true,
+		}, {
+			Name:           "Empty int slices",
+			Expected:       []int{},
+			Actual:         []int{},
+			ExpectedResult: true,
+		},
+		{
+			Name:           "Diff length in int slices",
+			Expected:       []int{1, 5, 6, 7, 3, 4, 5},
+			Actual:         []int{1, 2, 3},
+			ExpectedResult: false,
+		},
+		{
+			Name:           "Diff length in int slices",
+			Expected:       []int{1, 2, 3},
+			Actual:         []int{1, 5, 6, 7, 3, 4, 5},
+			ExpectedResult: false,
+		},
+		{
+			Name:           "Not equal int slices",
+			Expected:       []int{1, 5, 6, 7, 3, 4, 5},
+			Actual:         []int{1, 0, 0, 2, 3, 4, 5},
+			ExpectedResult: false,
+		},
+		{
+			Name:           "Equal float32 slices",
+			Expected:       []float32{1.25, 2.45, 3.7},
+			Actual:         []float32{1.25, 2.45, 3.7},
+			ExpectedResult: true,
+		},
+		{
+			Name:           "Diff float32 slices",
+			Expected:       []float32{1.25, 2.45, 3.7, 5.1},
+			Actual:         []float32{1.25, 2.2, 3.0, 4.0},
+			ExpectedResult: false,
+		},
+		{
+			Name:           "Equal float64 slices",
+			Expected:       []float64{1.25, 2.45, 3.7},
+			Actual:         []float64{1.25, 2.45, 3.7},
+			ExpectedResult: true,
+		},
+		{
+			Name:           "Diff float64 slices",
+			Expected:       []float64{1.25, 2.45, 3.7, 5.1},
+			Actual:         []float64{1.25, 2.2, 3.0, 4.0},
+			ExpectedResult: false,
+		},
+		{
+			Name:           "Equal bool slices",
+			Expected:       []bool{true, true, false},
+			Actual:         []bool{true, true, false},
+			ExpectedResult: true,
+		},
+		{
+			Name:           "Diff bool slices",
+			Expected:       []bool{true, true, false},
+			Actual:         []bool{true, false, false},
+			ExpectedResult: false,
+		},
+		{
+			Name:           "Equal string slices",
+			Expected:       []string{"aaa", "bbb", "ccc"},
+			Actual:         []string{"aaa", "bbb", "ccc"},
+			ExpectedResult: true,
+		},
+		{
+			Name:           "Not equal string slices",
+			Expected:       []string{"aaa", "bbb", "ccc"},
+			Actual:         []string{"aaa", "ccc", "bbb"},
+			ExpectedResult: false,
+		},
+		{
+			Name:           "Equal types of the slice([]interface{}) elements",
+			Expected:       []interface{}{1, "ccc", 3},
+			Actual:         []interface{}{1, "ccc", 3},
+			ExpectedResult: true,
+		},
+		{
+			Name:           "Different types of the slice([]interface{}) elements",
+			Expected:       []interface{}{1, 2, 3},
+			Actual:         []interface{}{1, "ccc", 3},
+			ExpectedResult: false,
+		},
+		{
+			Name:           "Equal [int]int maps",
+			Expected:       map[int]int{1: 1, 2: 2, 3: 3},
+			Actual:         map[int]int{1: 1, 2: 2, 3: 3},
+			ExpectedResult: false,
+		},
+		{
+			Name:           "Diff [int]int maps",
+			Expected:       map[int]int{1: 1, 2: 2, 3: 3},
+			Actual:         map[int]int{1: 1, 2: 5, 3: 7},
+			ExpectedResult: false,
+		},
+		{
+			Name:           "Diff [int]int maps with diff lengths",
+			Expected:       map[int]int{1: 1, 2: 2, 3: 3},
+			Actual:         map[int]int{1: 1},
+			ExpectedResult: false,
+		},
+		{
+			Name:           "Success with not slices - string",
+			Expected:       "string",
+			Actual:         "string",
+			ExpectedResult: true,
+		},
+		{
+			Name:           "Fail with not slices - string",
+			Expected:       "string",
+			Actual:         "strong",
+			ExpectedResult: false,
+		},
+		{
+			Name:           "Not slices - int",
+			Expected:       513,
+			Actual:         513,
+			ExpectedResult: false,
+		},
+	}
+
+	tt := &testing.T{}
+
+	for _, tc := range tcs {
+		t.Run(tc.Name, func(t *testing.T) {
+			isEqual := AssertEqualLists(tt, tc.Expected, tc.Actual)
+
+			assert.Equal(t, tc.ExpectedResult, isEqual)
+		})
+	}
+}
+
+func TestIsEmpty(t *testing.T) {
+	type TestCase struct {
+		Name     string
+		Object   interface{}
+		Expected bool
+	}
+
+	tcs := []TestCase{
+		{
+			Name:     "Empty int slice",
+			Object:   []int{},
+			Expected: true,
+		},
+		{
+			Name:     "Not empty int slice",
+			Object:   []int{1, 2, 3},
+			Expected: false,
+		},
+		{
+			Name:     "Empty int array with nulls",
+			Object:   [3]int{},
+			Expected: false,
+		},
+		{
+			Name:     "Not empty int array",
+			Object:   [3]int{1, 2, 3},
+			Expected: false,
+		},
+		{
+			Name:     "Empty string",
+			Object:   "",
+			Expected: true,
+		},
+		{
+			Name:     "Not empty string",
+			Object:   "string",
+			Expected: false,
+		},
+		{
+			Name:     "Nil map",
+			Object:   map[int]int{},
+			Expected: true,
+		},
+		{
+			Name:     "Empty map",
+			Object:   make(map[int]int, 0),
+			Expected: true,
+		},
+		{
+			Name:     "Not empty map",
+			Object:   map[int]int{1: 1},
+			Expected: false,
+		},
+		{
+			Name:     "Not list",
+			Object:   44,
+			Expected: false,
+		},
+	}
+
+	for _, tc := range tcs {
+		t.Run(tc.Name, func(t *testing.T) {
+			out := isListOrMapEmpty(tc.Object)
+
+			assert.Equal(t, tc.Expected, out)
+		})
+	}
+}
